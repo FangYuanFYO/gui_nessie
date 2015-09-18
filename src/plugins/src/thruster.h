9415ec68a9b1d76e3783b8877c90e73038ed120d
tfuhrmann <tomesman@gmail.com>
2015-09-18 07:58:23 -0400

    Add header for thruster plugin, DO NOT COMPILE.

---
+++ src/plugins/src/thruster.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <gazebo/gazebo.hh>
+#include <gazebo/math/Pose.hh>
+#include <gazebo/math/Vector3.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <gazebo/transport/transport.hh>
+#include <gazebo/msgs/msgs.hh>
+
+#include <boost/bind.hpp>
+
+#include <ros/ros.h>
+
+#include <string>
+
+namespace gazebo
+{
+	class ThrusterPlugin : public ModelPlugin
+	{
+	public:
+		ThrusterPlugin();
+		~ThrusterPlugin();
+
+	protected:
+		void ThrusterPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
+
+	private:	
+		ros::NodeHandle node_handler_;
+		ros::Subscriber subsriber_;
+		event::ConnectionPtr update_connection_;
+		std::string topic_name_;
+		physics::WorldPtr world_;
+		physics::ModelPtr model_;
+
+	};
+
+}
+
+/*************************************/
+/******* Links to keep in minde *******
+
+-> gazebo_ros_joint_state_publisher (robotNamespace)
+
+**************************************/
