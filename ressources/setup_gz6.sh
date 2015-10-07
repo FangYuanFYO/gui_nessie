351c5ac6626a8813f9066a54dab1f9e1aa2602bb
tfuhrmann <tomesman@gmail.com>
2015-10-07 07:44:51 -0400

    Updae setup_gzX.sh files for gui_nessie.

--- ressources/setup_gz6.sh
+++ ressources/setup_gz6.sh
@@ -2,9 +2,9 @@
 
 export GAZEBO_MASTER_URI=http://localhost:11345
 export GAZEBO_MODEL_DATABASE_URI=http://gazebosim.org/models
-export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-6.1:$SONIA_WORKSPACE_ROOT/simsonia-gazebo/ressources/
-export GAZEBO_PLUGIN_PATH=$SONIA_WORKSPACE_ROOT/simsonia-gazebo/devel/lib
-export GAZEBO_MODEL_PATH=$SONIA_WORKSPACE_ROOT/simsonia-gazebo/ressources/models
+export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-6.1:$SONIA_WORKSPACE_ROOT/gui_nessie/ressources/
+export GAZEBO_PLUGIN_PATH=$SONIA_WORKSPACE_ROOT/gui_nessie/devel/lib
+export GAZEBO_MODEL_PATH=$SONIA_WORKSPACE_ROOT/gui_nessie/ressources/models
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib/x86_64-linux-gnu/gazebo-6.1/plugins
 export OGRE_RESOURCE_PATH=/usr/lib/x86_64-linux-gnu/OGRE-1.9.0
 
